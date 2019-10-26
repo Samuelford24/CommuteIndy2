01f37f9dbbffbe8d58d923188addb5cdcd54464b
sford6 <samuelford48@gmail.com>
2019-10-26 13:24:19 -0400

    fdgdg

--- app/src/main/java/com/example/commuteindy/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/commuteindy/ui/gallery/GalleryFragment.java
@@ -13,23 +13,26 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.commuteindy.R;
+//myrides
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+   // private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+      //  galleryViewModel =
+              //  ViewModelProviders.of(this).get(GalleryViewModel.class);
+      View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+     //   final TextView textView = root.findViewById(R.id.text_gallery);
+      //  galleryViewModel.getText().observe(this, new Observer<String>() {
+           // @Override
+           // public void onChanged(@Nullable String s) {
+        //        textView.setText(s);
         return root;
     }
+
+       // });
+
+
 }
